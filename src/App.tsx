fc03d05e4cc1aa2b31eae60ceaf29c998c08b19c
sandvik16 <sannndvik16@gmail.com>
2022-04-04 22:29:31 +0300

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Tis is a title :)</h1>
+      <nav>
+        <li>home</li>
+        <li>contacts</li>
+        <li>articles</li>
+      </nav>
+      <h1>Tis is a title :)</h1>
       <span>subtitle</span>
     </div>
   );
